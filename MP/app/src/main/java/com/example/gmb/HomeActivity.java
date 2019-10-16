2c944b8df7c3892ce9988cb34c3d6f2931b9f4e4
kimms710 <kimms710@naver.com>
2019-10-17 02:41:04 +0900

    MP

---
+++ MP/app/src/main/java/com/example/gmb/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.example.gmb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        //상단 버튼 3개 인텐트
+        //홈
+        ImageButton homeButton = (ImageButton)findViewById(R.id.homeButton); // 홈 화면으로 이동
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Main Page", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(myintent);
+                finish();
+            }
+        });
+
+        //지도
+        ImageButton locwtButton = (ImageButton)findViewById(R.id.locwtButton); // 지도 화면으로 이동
+        locwtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Map Page", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(HomeActivity.this,MapActivity.class);
+                startActivity(myintent);
+                finish();
+            }
+        });
+
+        //메뉴
+        ImageButton menuwtButton = (ImageButton)findViewById(R.id.menuwtButton); // (미로그인시)로그인 화면으로 이동, (로그인된 경우)메뉴화면으로 이동
+        menuwtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Login Page", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(HomeActivity.this,LoginActivity.class);
+
+                startActivity(myintent);
+                finish();
+            }
+        });
+    }
+}
---
+++ MP/app/src/main/java/com/example/gmb/MapActivity.java
@@ -0,0 +1,54 @@
+package com.example.gmb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        //상단 버튼 3개 인텐트
+        //홈
+        ImageButton homeButton = (ImageButton)findViewById(R.id.homeButton); // 홈 화면으로 이동
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Main Page", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(MapActivity.this, MainActivity.class);
+                startActivity(myintent);
+                finish();
+            }
+        });
+
+        //지도
+        ImageButton locButton = (ImageButton)findViewById(R.id.locButton); // 지도 화면으로 이동
+        locButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Map Page", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(MapActivity.this,MapActivity.class);
+                startActivity(myintent);
+                finish();
+            }
+        });
+
+        //메뉴
+        ImageButton menuwtButton = (ImageButton)findViewById(R.id.menuwtButton); // (미로그인시)로그인 화면으로 이동, (로그인된 경우)메뉴화면으로 이동
+        menuwtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Login Page", Toast.LENGTH_LONG).show();
+                Intent myintent = new Intent(MapActivity.this,LoginActivity.class);
+
+                startActivity(myintent);
+                finish();
+            }
+        });
+    }
+}
